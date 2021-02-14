bd9a77c295a5e214a5959050def6d4c3cc64d028
Subramanyam Raju <subbu.balaraju@gmail.com>
2021-02-14 17:09:23 +0530

    - Changes in content view demo

---
+++ XFLab/Controls/ContentViewTemplate.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Xamarin.Forms;
+
+namespace XFLab.Controls
+{
+    public partial class ContentViewTemplate : ContentView
+    {
+
+        public static readonly BindableProperty PageTitleProperty = BindableProperty.Create(nameof(PageTitle), typeof(string), typeof(ContentViewTemplate), string.Empty);
+        public ContentViewTemplate()
+        {
+            InitializeComponent();
+        }
+
+        public string PageTitle
+        {
+            get => (string)GetValue(ContentViewTemplate.PageTitleProperty);
+            set => SetValue(ContentViewTemplate.PageTitleProperty, value);
+        }
+        public View Body
+        {
+            get => BodyContent.Content;
+            set => BodyContent.Content = value;
+        }
+        public View Footer
+        {
+            get => FooterContent.Content;
+            set => FooterContent.Content = value;
+        }
+    }
+}
