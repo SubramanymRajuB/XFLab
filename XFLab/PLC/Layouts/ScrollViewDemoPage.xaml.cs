1f0c95688dc0eeb6244ab0c94ccbccad01d8f1bf
Subramanyam Raju <subbu.balaraju@gmail.com>
2021-02-16 18:39:05 +0530

    - Changes in ScrollView Demo

--- XFLab/PLC/Layouts/ScrollViewDemoPage.xaml.cs
+++ XFLab/PLC/Layouts/ScrollViewDemoPage.xaml.cs
@@ -9,5 +9,16 @@ namespace FormsGallery.XamlExamples
         {
             InitializeComponent();
         }
+
+        void OnScrollViewScrolled(object sender, ScrolledEventArgs e)
+        {
+            Console.WriteLine($"ScrollX: {e.ScrollX}, ScrollY: {e.ScrollY}");
+        }
+
+        async void btn_Clicked(System.Object sender, System.EventArgs e)
+        {
+            //await scrollView.ScrollToAsync(0, 150, true);// Scroll a position into view
+            await scrollView.ScrollToAsync(label, ScrollToPosition.Start, true);
+        }
     }
 }
