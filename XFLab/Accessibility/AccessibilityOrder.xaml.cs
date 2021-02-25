b310cb1d49c1eec46071ee694bb3bfab7f833ccc
Subramanyam Raju <subbu.balaraju@gmail.com>
2021-02-25 15:15:09 +0530

    - Changes in Accessibility Demo

---
+++ XFLab/Accessibility/AccessibilityOrder.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XFLab.Accessibility
+{
+    public partial class AccessibilityOrder : ContentPage
+    {
+        public AccessibilityOrder()
+        {
+            InitializeComponent();
+        }
+    }
+}
